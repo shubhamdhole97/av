bd92b8bec04591606fb7d2fefc744ee60eed0c1c
shubhamdhole97 <shubhamdhole97@gmail.com>
2024-11-24 20:17:20 +0530

    first commit

---
+++ src/main/java/com/vtech/av/AvApplication.java
@@ -0,0 +1,13 @@
+package com.vtech.av;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtech/av/AvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtech.av;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
